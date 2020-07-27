80b8124982ccca71aad57656594b88e580338db4
Mark Rendle <mark@markrendle.net>
2020-07-27 19:43:32 +0100

    Add WorkspaceFailed handler

--- src/RoslynDemo/Program.cs
+++ src/RoslynDemo/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.MSBuild;
 
 namespace RoslynDemo
@@ -9,8 +11,12 @@ namespace RoslynDemo
         static async Task Main(string[] args)
         {
             MSBuildLocator.RegisterDefaults();
+
             var workspace = MSBuildWorkspace.Create();
             workspace.LoadMetadataForReferencedProjects = true;
+            
+            workspace.WorkspaceFailed += OnWorkspaceFailed;
+
             var solution = await workspace.OpenSolutionAsync(args[0]);
 
             await SyntaxQueryDemo.ListAsync(solution);
@@ -18,6 +24,11 @@ namespace RoslynDemo
             // await SymbolDemo.ListAsync(solution);
             // await DependenciesDemo.ListAllUsedTypes(solution);
             // await RewriterDemo.UpshiftAllStrings(workspace);
+        }
+
+        private static void OnWorkspaceFailed(object? sender, WorkspaceDiagnosticEventArgs e)
+        {
+            // Console.WriteLine(e.Diagnostic.Message);
         }
     }
 }
